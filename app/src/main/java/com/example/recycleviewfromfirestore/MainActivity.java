eb26f998d323a1588fe94eb656e17ff74fe6750b
Nouman <n.noumannoor@gmail.com>
2021-08-31 11:41:27 -0700

    with better design and ascending order

--- app/src/main/java/com/example/recycleviewfromfirestore/MainActivity.java
+++ app/src/main/java/com/example/recycleviewfromfirestore/MainActivity.java
@@ -4,11 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
@@ -20,11 +22,18 @@ public class MainActivity extends AppCompatActivity
     ArrayList<model> dataList;
     FirebaseFirestore db;
     myAdapter adapter;
+    ProgressDialog progressDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setCancelable(false);
+        progressDialog.setMessage("Fetching Data.....");
+        progressDialog.show();
+
 
         recyclerView=(RecyclerView)findViewById(R.id.recycleViewEditText);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -33,17 +42,21 @@ public class MainActivity extends AppCompatActivity
         recyclerView.setAdapter(adapter);
 
         db=FirebaseFirestore.getInstance();
-        db.collection("Users").get()
+
+        db.collection("Users").orderBy("FirstName", Query.Direction.ASCENDING).get()
                 .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
                     @Override
                     public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
                         List<DocumentSnapshot> list=queryDocumentSnapshots.getDocuments();
+                        if (progressDialog.isShowing()) progressDialog.dismiss();
+
                         for(DocumentSnapshot d:list)
                         {
                             model obj=d.toObject(model.class);
                             dataList.add(obj);
                         }
                         adapter.notifyDataSetChanged();
+                        if (progressDialog.isShowing()) progressDialog.dismiss();
                     }
                 });
     }
